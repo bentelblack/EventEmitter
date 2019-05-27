9bb70405b5c0588f283f40832544f4ab317a157b
blackbentel <33181771+bentelblack@users.noreply.github.com>
2019-05-27 11:42:44 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,39 @@
+#EventEmitter
+
+### 介绍 EventEmitter 类中的方法
+
+1.  `$on` 注册事件
+    - 参数 1： 事件名称； 参数 2. 回调函数
+    - 当没有该事件名就注册一个该事件名，并将参数 2 作为该事件的回调函数
+    - 但该回调函数存在则删除之前的回调函数并用参数 2 代替
+    - 参数 2 若为匿名函数则不支持移除该回调
+2.  `$emit` 触发函数
+    - 参数 1： 事件名称； 参数 2：回调函数传入的参数
+    - 调用该方法会触发该事件下的所有回调函数
+3.  `$remove` 移除事件或者移除事件下的某个回调函数
+
+    - 参数 1： 事件名称； 参数 2：事件的某个回调（可选）
+    - 参数 2 存在的情况下是移除事件下的回调函数
+    - 参数 2 不存在的情况下移除这个事件
+
+4.  `$once` 注册事件并再其第一次被触发后移除这个事件
+
+    - 参数 1： 事件名称； 参数 2： 事件的回调函数
+
+### 使用 EventEmitter
+
+1.  实例化 EventEmitter
+    `const event = new EventEmitter`
+2.  使用
+    ```
+        const logText = function (text = '日志') {
+            console.log(text)
+        }
+        event.$on('log', logText)
+        event.$emit('log') // '日志'
+        event.$remove('log')
+        event.$emit('log') // 该事件不存在
+        event.$once('log2', logText)
+        event.$emit('log2') // 日志
+        event.$emit('log2') // 该事件不存在
+    ```
--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ class EventEmiter {
   constructor() {
     this.events = {};
   }
-  on(eventName, eventFunc) {
+  $on(eventName, eventFunc) {
     if (!eventName) return console.error("事件名不能为空");
     if (!eventFunc) return console.error("事件回调函数不能为空");
     const _event = this.events[eventName] || [];
-    const eventHasThisFunc = _event.some((item, index) => {
-      if (item === eventFunc) {
-        _event.splice(index, 1, eventFunc);
-        return true;
-      }
-      return false;
-    });
-    !eventHasThisFunc && _event.push(eventFunc);
+    if (_event.length === 0) {
+      return (this.events[eventName] = [eventFunc]);
+    }
+
+    (_event.includes(eventFunc) &&
+      _event.splice(_event.indexOf(eventFunc), 1, eventFunc)) ||
+      _event.push(eventFunc);
     this.events[eventName] = _event;
   }
-  emit(eventName, ...args) {
+  $emit(eventName, ...args) {
     if (!eventName) return console.error("事件名不能为空");
+    if (!this.events[eventName]) return console.error("该事件不存在");
     const _event = this.events[eventName] || [];
     if (_event.length === 0) {
       return console.error("该事件无回调函数");
@@ -26,40 +26,39 @@ class EventEmiter {
       item && item(...args);
     });
   }
-  remove(eventName, eventFunc) {
+  $remove(eventName, eventFunc) {
     if (!eventName) return console.error("事件名不能为空");
-    if (!eventFunc) return console.error("事件回调函数不能为空");
+    if (!eventFunc) {
+      return (
+        (this.events[eventName] && delete this.events[eventName]) ||
+        console.error("事件列表中没有此事件")
+      );
+    }
     const _event = this.events[eventName] || [];
-    const eventHasThisFunc = _event.some((item, index) => {
-      if (item === eventFunc) {
-        _event.splice(index, 1);
-        console.log("移除回调函数成功");
-        return true;
-      }
-      return false;
-    });
-    !eventHasThisFunc && console.error("事件中没有该回调函数");
+    (_event.includes(eventFunc) &&
+      _event.splice(_event.indexOf(eventFunc), 1)) ||
+      console.error("事件中没有该回调函数");
     this.events[eventName] = _event;
   }
-  delete(eventName) {
-    if (!eventName) return console.error("事件名不能为空");
-    (this.events[eventName] && delete this.events[eventName]) ||
-      console.error("事件列表中没有此事件");
-  }
-  once(eventName, eventFunc) {
+  $once(eventName, eventFunc) {
     if (!eventName) return console.error("事件名不能为空");
     if (!eventFunc) return console.error("事件回调函数不能为空");
     const cb = (...args) => {
       eventFunc && eventFunc(...args);
-      this.delete(eventName);
+      this.$remove(eventName);
     };
-    this.on(eventName, cb);
+    this.$on(eventName, cb);
   }
 }
 const event = new EventEmiter();
-function logText(text = "我是日志") {
+const logText = function(text = "日志") {
   console.log(text);
-}
-event.once("log", logText);
-event.emit("log");
-event.emit("log");
+};
+event.$on("log", logText);
+console.log(event);
+event.$emit("log"); // '日志'
+event.$remove("log");
+event.$emit("log"); // 事件列表中没有此事件
+event.$once("log2", logText);
+event.$emit("log2");
+event.$emit("log2"); // 事件列表中没有此事件
